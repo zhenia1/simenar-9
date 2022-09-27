dc8c92d21a62c7ffb70b672dae2b2ba9417f2b95
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-28 03:35:31 +0500

    task1

--- obj/Debug/net6.0/task1.AssemblyInfo.cs
+++ obj/Debug/net6.0/task1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("task2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("task2")]
+[assembly: System.Reflection.AssemblyProductAttribute("task1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
